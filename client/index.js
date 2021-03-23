f5691a4a531b3209fbe3e6a291899fd328443642
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-03-23 13:04:59 -0300

    finished second day :tada:

--- client/index.js
+++ client/index.js
@@ -1,13 +1,20 @@
 // Importa a class events do Node.js
-import Events from 'events';
-import TerminalController from './src/terminalController.js';
+import Events from "events";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
+import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv;
+
+const config = CliConfig.parseArguments(commands);
 
 // Cria o evento componentEmitter, que será responsável em pegar a camada controladora e outras camadas, emitindo os eventos necessários.
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
 
-//Cria o Terminal Controller 
-const controller = new TerminalController();
+// //Cria o Terminal Controller
+// const controller = new TerminalController();
 
-// Inicia o projeto projeto
-await controller.initializeTable(componentEmitter);
-
+// // Inicia o projeto projeto
+// await controller.initializeTable(componentEmitter);
