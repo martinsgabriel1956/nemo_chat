009d1e0591d06b985999a1ac7f8b743e8bd0440d
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-03-23 13:03:39 -0300

    finished second_day :tada:

---
+++ server/src/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+  event: {
+    MESSAGE: 'message',
+    NEW_USER_CONNECTED: 'new_user_connected',
+    DISCONNECT_USER: 'disconnectUser',
+    UPDATE_USERS: 'updateUsers'
+  }
+}
